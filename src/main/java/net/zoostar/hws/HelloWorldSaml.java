365b51f84f05b08675dc516ecb0e51968a5b0c3f
Aarnav Shah <aarnav@zoostar.net>
2024-04-09 16:32:51 -0400

    HWS-2: Integrate with Google SAML

--- src/main/java/net/zoostar/hws/HelloWorldSaml.java
+++ src/main/java/net/zoostar/hws/HelloWorldSaml.java
@@ -1,8 +1,12 @@
 package net.zoostar.hws;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -17,6 +21,13 @@ public class HelloWorldSaml {
 		log.info("{}...", "Loading Swagger configuration");
 		return new OpenAPI().info(new Info().title("Hello World SAML")
 				.description("Example Project for SAML2 Integration with Spring Boot Project."));
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+		return security
+				.authorizeRequests(authorize -> authorize.antMatchers("/").permitAll().anyRequest().authenticated())
+				.saml2Login(withDefaults()).build();
 	}
 
 	public static void main(String[] args) {
